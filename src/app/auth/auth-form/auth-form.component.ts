fe545bd34d9cd5da5fb1a6cef4f9da8f5add2338
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-02-09 20:46:02 -0500

    added connection and visual between sign up and log in page

--- src/app/auth/auth-form/auth-form.component.ts
+++ src/app/auth/auth-form/auth-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-form.component.css']
 })
 export class AuthFormComponent implements OnInit {
+  form: FormGroup;
+  formTitle: string;
+  currentRoute: string;
+  navStatement: string;
+  navLink: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private router: Router) {
+    this.currentRoute = route.snapshot.url[0].path;
+   }
 
   ngOnInit(): void {
+    this.onSwitchMode();
+    this.form =  this.fb.group({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required),
+      'confirmPassword': new FormControl(null, Validators.required),
+      'displayName': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSwitchMode() {
+    if (this.currentRoute === 'login') {
+      this.formTitle = 'Welcome Back! Sign In.';
+      this.navStatement = "No account? ";
+      this.navLink = "Sign up.";
+    } else if (this.currentRoute === 'signup') {
+      this.formTitle = 'Create an Account!';
+      this.navStatement = "Have an account? ";
+      this.navLink = "Sign in.";
+    }
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('hello');
+    console.log(form.value);
+    this.router.navigate(['home-page']);
   }
 
 }
