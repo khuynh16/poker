a028f36fd20a17d451a8329c22f9402d5af8161e
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-02-12 02:04:21 -0500

    added game settings UI page; issue with ng-deep affecting other components, need to fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,7 +46,7 @@ import { GameSettingsComponent } from './game-settings/game-settings.component';
     FormsModule,
     MatInputModule,
     MatIconModule,
-    MatTooltipModule
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game-settings/game-settings.component.ts
+++ src/app/game-settings/game-settings.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-game-settings',
   templateUrl: './game-settings.component.html',
-  styleUrls: ['./game-settings.component.css']
+  styleUrls: ['./game-settings.component.css'],
 })
 export class GameSettingsComponent implements OnInit {
+  form: FormGroup;
+  selectedValue: string;
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.form =  this.fb.group({
+      'numOfBots': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit(form) {
+    this.router.navigate(['game-floor']);
   }
 
 }
