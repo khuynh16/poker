913dc64163b3c5fd9aa508f613b4058410cfe7fa
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-02-13 02:09:39 -0500

    commenting how poker plays out; pokerGame service needed

--- src/app/game-floor/game-floor.component.ts
+++ src/app/game-floor/game-floor.component.ts
@@ -17,6 +17,28 @@ export class GameFloorComponent implements OnInit {
   gameStart() {
     console.log('Game started.');
     this.cardsInHandDisplayed = true;
+
+    /*
+      Assign dealer:
+        -on first round, randomly assign; afterwards, dealer should be moved to the right
+          of previous dealer
+      
+      Conduct small blind calculation from person left of dealer:
+
+      Conduct large blind calculation from person right of the small blind player
+
+      Conduct gameplay:
+        -start with player right of large blind player
+        -they are given three options:  
+          -call (equal to large blind amount)
+          -raise (increasing the pot from the large amount)
+          -fold (toss hand away and game continues with remaining players)
+        -if call or raise, gameplay goes to next player on the right
+        -if every player ends up calling to a certain amount, without no one re-raising,
+        game goes into the flop (the first three community cards are displayed)
+        -rinse and repeat, where game goes into the turn (4th card) and river (5th card)
+      
+    */
   }
 
   displayFlop() {
