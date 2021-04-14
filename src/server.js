d5b55b91d6729d0462f14fca4010370b8df4001f
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-04-14 02:24:37 -0400

    changed to match proper angular.json name

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/game-of-poker'));
+app.use(express.static(__dirname + '/dist/poker'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/game-of-poker/index.html'));
+res.sendFile(path.join(__dirname+'/dist/poker/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
