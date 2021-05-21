3c60989c72a33d36e70eab9c54dc06f3fb6335aa
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-05-21 00:22:24 -0400

    updated path to server.js

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/poker'));
+app.use(express.static('dist/poker'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/poker/index.html'));
+    res.sendFile(path.join('dist/poker/index.html'));
 });
 
 console.log('hello');
